38fe6ef2532bd9fba31eceeafa766a7fdd27dab9
RockThunderheart <alexdaigre@gmail.com>
2018-09-09 14:44:57 -0500

    generated various files with gcc

--- main.c
+++ main.c
@@ -7,7 +7,7 @@ int main (int argc, char *argv[]) {
     int i, n;
 
     if (argc != 2) {
-        fprintf(stderr, "usage: %s processes \n", argv[0])
+        fprintf(stderr, "usage: %s processes \n", argv[0]);
         return 1;
     }
 

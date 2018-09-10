be9ae22dc5d9f731a41f1c856fbed893ac016d08
RockThunderheart <alexdaigre@gmail.com>
2018-09-09 23:28:58 -0500

    added stuff for eexercize 3.8

--- main.c
+++ main.c
@@ -2,12 +2,13 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include<sys/wait.h> 
 
 char* concat(const char *s1, const char *s2);
 
 int main (int argc, char *argv[]) {
     pid_t childpid = 0;
-    int i, n;
+    int i, n, j, m, k;
     const char helpOption[] = "-h";
     const char testErrorOption[] = "-p";
     const char getPidsOption[] = "-n";
@@ -18,14 +19,16 @@ int main (int argc, char *argv[]) {
     }
 
     if ((!strcmp(argv[1], helpOption)) && (argc == 2)) {
-        fprintf(stderr, "Command Options\n Help: %s -h  \n Test Error: %s -p  \n Create forking processes: %s -n PID \n", argv[0], argv[0], argv[0]);
+        fprintf(stderr, "Command Options\n Help: %s -h  \n Test Error: %s -p  \n Create forking processes: %s -n PID loops delay \n", argv[0], argv[0], argv[0]);
         return 0;
     } else if ((!strcmp(argv[1], testErrorOption)) && (argc == 2)) {
         perror(concat(argv[0], ": Error: Detailed error message"));
         // perror("error");
         return 1;
-    } else if ((!strcmp(argv[1], getPidsOption)) && (argc == 3)) {
+    } else if ((!strcmp(argv[1], getPidsOption)) && (argc == 5)) {
         n = atoi(argv[2]);
+        k = atoi(argv[3]);
+        m = atoi(argv[4]);
 
         for (i = 1; i < n; i++) {
             if (childpid = fork()){
@@ -33,9 +36,13 @@ int main (int argc, char *argv[]) {
             }
         }
 
-        fprintf(stderr, "i:%d process ID:%ld parent ID:%ld child ID:%ld\n",
-                i, (long)getpid(), (long)getppid(), (long)childpid);
-        
+        for (j = 0; j <= k; j++) {
+            wait(NULL);
+            fprintf(stderr, "i:%d process ID:%ld parent ID:%ld child ID:%ld\n",
+                    i, (long)getpid(), (long)getppid(), (long)childpid);
+            sleep(m);
+        }
+
         return 0;  
     } else {
         fprintf(stderr, "Incorrect options. For help: %s -h  \n", argv[0]);
